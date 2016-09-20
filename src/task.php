bdf46c5f4c8dd90f66f7b459d0e47a3aca77e912
Molly LeCompte and Tim Bourgault <molly@>
2016-09-20 10:49:19 -0700

    integrate tasks and categories.

--- src/task.php
+++ src/task.php
@@ -2,19 +2,15 @@
 class Task
 {
     private $description;
+    private $category_id;
     private $id;
 
-    function __construct($description, $id = null)
+    function __construct($description, $id = null, $category_id)
     {
         $this->description = $description;
         $this->id = $id;
+        $this->category_id = $category_id;
     }
-
-    function getId()
-    {
-        return $this->id;
-    }
-
     function setDescription($new_description)
     {
       $this->description = (string) $new_description;
@@ -22,12 +18,23 @@ class Task
 
     function getDescription()
     {
-      return $this->description;
+        return $this->description;
     }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function getCategoryId()
+    {
+        return $this->category_id;
+    }
+
 
     function save()
     {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()});");
         $this->id = $GLOBALS['DB']->lastInsertId();
     }
 
@@ -38,7 +45,8 @@ class Task
       foreach($returned_tasks as $task) {
           $description = $task['description'];
           $id = $task['id'];
-          $new_task = new Task($description, $id);
+          $category_id = $task['category_id'];
+          $new_task = new Task($description, $id, $category_id);
           array_push($tasks, $new_task);
       }
       return $tasks;
